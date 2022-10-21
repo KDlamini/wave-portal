11b66849c8eb7eef8e5d6bc82c00375fa005c4f2
Simo Nkosi <wilzmar.kwd@gmail.com>
2022-10-21 10:57:16 +0200

    Add Wave function

--- contracts/WavePortal.sol
+++ contracts/WavePortal.sol
@@ -5,7 +5,14 @@ pragma solidity ^0.8.17;
 import "hardhat/console.sol";
 
 contract WavePortal {
+    uint256 totalWaves;
+
     constructor() {
         console.log("Hi there! I am Simo. This is my smart contract wave portal");
+    }
+
+    function wave() public {
+        totalWaves += 1;
+        console.log("%s has waved!", msg.sender);
     }
 }
