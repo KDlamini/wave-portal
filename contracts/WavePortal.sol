2a284d43d263799a497da525f7fdae31e32cbf5f
Simo Nkosi <wilzmar.kwd@gmail.com>
2022-10-26 10:16:44 +0200

    Add Pokemon methods

--- contracts/WavePortal.sol
+++ contracts/WavePortal.sol
@@ -6,9 +6,27 @@ import "hardhat/console.sol";
 
 contract WavePortal {
     uint256 totalWaves;
+    mapping(address => string) public pokemons;
+    mapping(address => uint256) public pokemonIndex;
 
     constructor() {
-        console.log("Hi there! I am Simo. This is my smart contract wave portal");
+        console.log("Hi there! I am Simo. Say hi with your favorite pokemon!");
+    }
+
+    function getPokemon(address _address) public view returns (string memory) {
+        return pokemons[_address];
+    }
+
+    function setPokemon(address _address, string memory _pokemon) public {
+        pokemons[_address] = _pokemon;
+    }
+
+    function getPokemonIndex(address _address) public view returns (uint256) {
+        return pokemonIndex[_address];
+    }
+
+    function setPokemonIndex(address _address, uint256 _index) public {
+        pokemonIndex[_address] = _index;
     }
 
     function wave() public {
