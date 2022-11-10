03866274438d14c3952e86db5f0b50666d99859e
Simo Nkosi <wilzmar.kwd@gmail.com>
2022-11-10 14:54:19 +0200

    Update deploy ETH value

--- scripts/deploy.js
+++ scripts/deploy.js
@@ -7,7 +7,7 @@ const main = async () => {
   
     const waveContractFactory = await hre.ethers.getContractFactory("SmartPortal");
     const waveContract = await waveContractFactory.deploy({
-      value: hre.ethers.utils.parseEther("0.005"),
+      value: hre.ethers.utils.parseEther("0.001"),
     });
   
     await waveContract.deployed();
